18cb9b2c54cb0ccc1bfe18eaa2042614fdbad5f0
Filip Farkasovsky <85394252+FilipFarkasovsky@users.noreply.github.com>
2024-09-11 00:35:26 +0200

    Added UI

--- ChatApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,38 +1,137 @@
 ﻿using ChatClient.MVVM.Core;
 using ChatClient.MVVM.Model;
 using ChatClient.Net;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Printing;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace ChatClient.MVVM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
         public ObservableCollection<UserModel> Users { get; set; }
-        public ObservableCollection<string> Messages { get; set; }
+        // public ObservableCollection<string> Messages { get; set; }
         public RelayCommand ConnectToServerCommand { get; set; }
         public RelayCommand SendMessageCommand { get; set; }
         private Server server;
-        public string Message { get; set; }
+        //public string Message { get; set; }
         public string Username { get; set; }
+
+
+
+
+
+
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+        private string _message;
+        public RelayCommand SendCommand { get; set; }
+
+        public string Message
+        {
+            get { return _message; }
+            set 
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+        private ContactModel _selectedContact;
+            
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set 
+            { 
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public MainViewModel()
         {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel()
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true,
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false,
+
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "bunnz",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "bunnz",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Last",
+                Time = DateTime.Now,
+                FirstMessage = true,
+                IsNativeOrigin = false,
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel()
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                });
+            }
+
+
             Users = new ObservableCollection<UserModel>();
-            Messages = new ObservableCollection<string>();
-           server = new Server();
+            //Messages = new ObservableCollection<string>();
+            server = new Server();
             server.connectedEvent += UserConnected;
             server.msgReceivedEvent += MessageReceived;
             server.userDisconnectedEvent += RemoveUser;
-           ConnectToServerCommand = new RelayCommand(o => server.ConnectToServer(Username), o=> !string.IsNullOrEmpty(Username));
+            ConnectToServerCommand = new RelayCommand(o => server.ConnectToServer(Username), o=> !string.IsNullOrEmpty(Username));
             SendMessageCommand = new RelayCommand(o => server.SendMesssageToServer(Message), o=> !string.IsNullOrEmpty(Message));
         }
 
@@ -45,7 +144,7 @@ namespace ChatClient.MVVM.ViewModel
         private void MessageReceived()
         {
             var msg = server.packetReader.ReadMessage();
-            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+            //Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
         }
 
         private void UserConnected()
